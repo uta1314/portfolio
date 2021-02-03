cf0c354658a21f760b19e83588bf50231fd8d4dd
uta <jsmoraru13@gmail.com>
2021-02-03 21:03:53 +0900

    Fix: Font Awesomeから使用しないアイコンを削除

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -4,12 +4,12 @@ import router from './router'
 
 /* Font Awesome */
 import {library} from '@fortawesome/fontawesome-svg-core'
-import {fas} from '@fortawesome/free-solid-svg-icons'
-import {far} from '@fortawesome/free-regular-svg-icons'
-import {fab} from '@fortawesome/free-brands-svg-icons'
+//import {fas} from '@fortawesome/free-solid-svg-icons'
+//import {far} from '@fortawesome/free-regular-svg-icons'
+import {faHtml5, faCss3Alt, faJsSquare, faPhp, faJava, faVuejs} from '@fortawesome/free-brands-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
-library.add(fas, far, fab)
+library.add(faHtml5, faCss3Alt, faJsSquare, faPhp, faJava, faVuejs)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
